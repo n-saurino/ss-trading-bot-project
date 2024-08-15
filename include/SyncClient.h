7ff4d7f6bbba88f3cb72a1a755d68485856af028
Nigel Saurino <nigelsaurino@gmail.com>
2024-08-15 16:12:35 -0500

    Starting client for the SMA trading bot project

---
+++ include/SyncClient.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <boost/asio.hpp>
+#include <iostream>
+#include <vector>
+
+using boost::asio::ip::tcp;
+
+class Client
+{
+private:
+    /* data */
+    std::vector<char> buf_;
+    int Start();
+public:
+    Client(/* args */);
+    ~Client();
+};
+
+Client::Client(/* args */)
+{
+    Start();
+}
+
+Client::~Client()
+{
+}
+
+int Client::Start(){
+    // create context
+    boost::asio::io_context io;
+    std::string hostname = "127.0.0.1";
+    tcp::resolver resolver(io);
+    // A resolver takes a host name and service name and turns them into a list of endpoints. 
+    // We perform a resolve call using the name of the server, specified in argv[1], 
+    // and the name of the service, in this case "daytime".
+    tcp::resolver::results_type endpoints = resolver.resolve(hostname, "13");
+    tcp::socket socket(io);
+    boost::asio::connect(socket, endpoints);
+
+    while(true){
+        std::vector<char> buf(128);
+        boost::system::error_code error;
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+        if(error == boost::asio::error::eof){
+            break; // Connection closed cleanly by peer.
+        }else if (error){
+            throw boost::system::system_error(error);
+        }
+
+        std::cout.write(buf.data(), len);
+    }
+
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../include/SyncClient.h"
+
+int main(int, char**){
+    Client client;
+}
